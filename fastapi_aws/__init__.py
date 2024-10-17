4fe6fc5b711fd9ca8354a5c316abed26fa87bb42
ed <anax@hotmail.co.uk>
2024-10-17 14:12:43 +0100

    ci

--- fastapi_aws/__init__.py
+++ fastapi_aws/__init__.py
@@ -1,3 +1,5 @@
+from ._version import version as __version__
+
 from .authorizers import CognitoAuthorizer, APIKeyAuthorizer, LambdaAuthorizer
 from .route import AWSAPIRoute
 from .router import AWSAPIRouter
